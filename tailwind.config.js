669ed6b3d86e3ac60b1b69c1f91cb4186c2b99bf
Zankcry <101846104+Zankcry@users.noreply.github.com>
2025-09-02 19:12:31 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000',
+        secondary: '#181818',
+        accent: '#ecc94b',
+        text: '#ffffff',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
